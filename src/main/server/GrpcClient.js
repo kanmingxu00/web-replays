0f7e33dea9b71bf710e30f2bb0e0b90f3e81d6ed
Kanming Xu <kanmingwx@gmail.com>
2020-04-06 06:35:44 -0400

    Worked a bit on GrpcClient

--- src/main/server/GrpcClient.js
+++ src/main/server/GrpcClient.js
@@ -1,5 +1,6 @@
-import { ParseClient } from '../../protos/parse_grpc_web_pb.js';
-import { MatchRequest, MatchResponse } from '../../protos/parse_pb.js';
+var { ParseClient } = require('../../protos/parse_grpc_web_pb.js')
+var { MatchRequest, MatchResponse } = require('../../protos/parse_pb.js');
+var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
 
 params = {
     address: "localhost:8080",
@@ -18,26 +19,37 @@ const run = (/* */) => {
     }
 }
 
-export default async function(matchID) {
-    const parseClient = new ParseClient(address)
-    const request = new MatchRequest()
+var func = async function(matchID) {
+    
+
+    const parseClient = new ParseClient(params.address);
+    const request = new MatchRequest();
     const d = new Date();
     const clientID = d.getTime();
     request.setClientid(clientID);
     request.setMatchid(matchID);
 
-    parseClient.createStream(request)
+    console.log("watashi")
+
+    // i had to dig through code for this: (trust me on it)
+    // https://github.com/grpc/grpc-web/blob/master/javascript/net/grpc/web/grpcwebclientreadablestream.js
+    const gay = parseClient.createStream(request, {}); // returns a grpcwebclientreadablestream
+    console.log(gay)
+
+    // const stream = 
 
     // stream = some stream
     // const promise = new Promise((/* Stream */) => {
     //     while(true) {
     //         // returns when you get ready message
     //     }
-    // }) 
-    await readyMessage(/* Stream */)
-    run(/* Stream */)
+    // // }) 
+    // await readyMessage(/* Stream */)
+    // run(/* Stream */)
 
 }
+
+func()
 
 
 
