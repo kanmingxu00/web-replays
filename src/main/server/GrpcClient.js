f505ca6b55c95f527f70f20643e4bd2a7008913f
Kanming Xu <kanmingwx@gmail.com>
2020-04-05 21:57:00 -0400

    OVerview of the client comm

--- src/main/server/GrpcClient.js
+++ src/main/server/GrpcClient.js
@@ -1,18 +1,48 @@
-import {ParseClient} from '../../protos/parse_grpc_web_pb.js';
-import {MatchRequest, MatchResponse} from '../../protos/parse_pb.js';
-import Date
+import { ParseClient } from '../../protos/parse_grpc_web_pb.js';
+import { MatchRequest, MatchResponse } from '../../protos/parse_pb.js';
 
 params = {
     address: "localhost:8080",
     defaultName: "world"
 }
 
-let parseClient = new ParseClient(address)
-
-const log = (word) => {
-    console.log(word)
+const readyMessage = (/* Stream */) => {
+    while(true) {
+        // returns when you get ready message
+    }
 }
 
-function main() {
+const run = (/* */) => {
+    while(true) {
+        // keep reading
+    }
+}
+
+export default async function(matchID) {
+    const parseClient = new ParseClient(address)
+    const request = new MatchRequest()
+    const d = new Date();
+    const clientID = d.getTime();
+    request.setClientid(clientID);
+    request.setMatchid(matchID);
+
+    parseClient.createStream(request)
+
+    // stream = some stream
+    // const promise = new Promise((/* Stream */) => {
+    //     while(true) {
+    //         // returns when you get ready message
+    //     }
+    // }) 
+    await readyMessage(/* Stream */)
+    run(/* Stream */)
 
 }
+
+
+
+
+
+
+
+
